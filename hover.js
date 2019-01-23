0eee332b78e72ec6a97dfcf820553055f7f5ee85
Aditya Priyam <apriyam9@gmail.com>
2019-01-23 18:31:17 +0530

    first commit

---
+++ hover.js
@@ -0,0 +1,83 @@
+		//Methods illustrating hover techniques
+
+
+		function kodeathonHover(element) {
+			// body...
+			element.setAttribute('src','./asset/nameH.png')
+		}
+		function kodeathonUnHover(element) {
+			// body...
+			element.setAttribute('src','./asset/name.png')
+		}
+		function onHover1(element) {
+			// body...
+			element.setAttribute('src','./asset/q1H.png')
+		}
+		function unHoverRule(element) {
+			// body...
+			element.setAttribute('src','./asset/rules.png')
+		}
+		function onHoverRule(element) {
+			// body...
+			element.setAttribute('src','./asset/rulesH.png')
+		}
+		function unHoverQ(element) {
+			// body...
+			element.setAttribute('src','./asset/qstn.png')
+		}
+		function onHoveQ(element) {
+			// body...
+			element.setAttribute('src','./asset/qstnH.png')
+		}
+		function unHover1(element) {
+			// body...
+			element.setAttribute('src','./asset/q1.png')
+		}
+		function onHover2(element) {
+			// body...
+			element.setAttribute('src','./asset/q2H.png')
+		}
+		function unHover2(element) {
+			// body...
+			element.setAttribute('src','./asset/q2.png')
+		}
+		function onHover3(element) {
+			// body...
+			element.setAttribute('src','./asset/q3H.png')
+		}
+		function unHover3(element) {
+			// body...
+			element.setAttribute('src','./asset/q3.png')
+		}
+		function onHover4(element) {
+			// body...
+			element.setAttribute('src','./asset/q4H.png')
+		}
+		function unHover4(element) {
+			// body...
+			element.setAttribute('src','./asset/q4.png')
+		}
+		function onHover5(element) {
+			// body...
+			element.setAttribute('src','./asset/q5H.png')
+		}
+		function unHover5(element) {
+			// body...
+			element.setAttribute('src','./asset/q5.png')
+		}
+		function onHover6(element) {
+			// body...
+			element.setAttribute('src','./asset/q6H.png')
+		}
+		function unHover6(element) {
+			// body...
+			element.setAttribute('src','./asset/q6.png')
+		}
+		function onHover7(element) {
+			// body...
+			element.setAttribute('src','./asset/q7H.png')
+		}
+		function unHover7(element) {
+			// body...
+			element.setAttribute('src','./asset/q7.png')
+		}
